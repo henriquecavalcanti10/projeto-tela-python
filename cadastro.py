8a4788696ca3826b8d2becff9af2e2ad789a033b
Henrique Oliveira <95507337+henriquecavalcanti10@users.noreply.github.com>
2022-03-31 18:16:23 -0300

    rodando

--- cadastro.py
+++ cadastro.py
@@ -2,14 +2,14 @@ from PySimpleGUI import PySimpleGUI as sg # importando a bibli
 
  # layout
 sg.theme('Reddit') # tema da janela
-layout =[ # criando os layouts da tela
-    [sg.Text('Usuário'),sg.Input(key='usuario')],
-    [sg.Text('Senha),sg.Input(key='senha,passord_char'*')],
+layout = [
+    [sg.Text('Usuário'), sg.Input(key='usuario' ,size=(30,1))],
+    [sg.Text('Senha'), sg.Input(key='senha', password_char='*', size=(30,1))],  # criando os layouts da tela
     [sg.Checkbox('Salvar o login')],
     [sg.Button('Entrar')]
 ]
  # janela
-janela = sg.Windows('Tela de Login', layout)
+janela = sg.Window('Tela de Login', layout)
  # ler os eventos
 while True:
     eventos , valores = janela.read()
