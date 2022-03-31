31de0a01fd4eb4c334f596aa10a2d96753c93b76
Henrique Oliveira <95507337+henriquecavalcanti10@users.noreply.github.com>
2022-03-31 18:08:48 -0300

    att

--- cadastro.py
+++ cadastro.py
@@ -2,7 +2,7 @@ from PySimpleGUI import PySimpleGUI as sg # importando a bibli
 
  # layout
 sg.theme('Reddit') # tema da janela
-layout = [ # criando os layouts da tela
+layout =[ # criando os layouts da tela
     [sg.Text('Usuário'),sg.Input(key='usuario')],
     [sg.Text('Senha),sg.Input(key='senha,passord_char'*')],
     [sg.Checkbox('Salvar o login')],
@@ -16,5 +16,5 @@ while True:
     if eventos == sg.WINDOW_CLOSED:
         break
     if eventos == 'Entrar':
-        if valores['usuario'] == 'henrique' and valores['senha'] =='123456'
-        print('Bem vindo')
+        if valores['usuario'] == 'henrique' and valores['senha'] =='123456':
+         print('Bem vindo')
